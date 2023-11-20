5217ea452ddb0b9fe57f5fe385552642005749cc
discoverablemarketing <chris@discoverablemarketing.com>
2023-11-20 16:55:31 -0500

    Untrack .DS_Store and _site/

--- _site/assets/js/codebox.js
+++
@@ -1,9 +0,0 @@
-function copyCodeToClipboard(elementId) {
-  const codeSnippet = document.getElementById(elementId);
-  const textArea = document.createElement("textarea");
-  textArea.value = codeSnippet.textContent;
-  document.body.appendChild(textArea);
-  textArea.select();
-  document.execCommand("copy");
-  document.body.removeChild(textArea);
-}
